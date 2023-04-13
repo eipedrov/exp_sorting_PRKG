e73fd20240560231217615cc6faece0ed5279ba3
Rafael Nobrega <rafaelnobregadelima2809@gmail.com>
2023-04-12 21:09:16 -0400

    teste

--- Commit.bat
+++ Commit.bat
@@ -1,5 +1,6 @@
 @echo off
 set /p mensagem=Digite a mensagem de commit: 
+set /p origem=Digite a origem: 
 git add .
 git commit -m "%mensagem%"
-git push -u origin main
+git push -u origin %origem%
